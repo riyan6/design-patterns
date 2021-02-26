368dc6d61d48fe505af244c0a91eed58908fb339
riyan6 <751696921@qq.com>
2021-02-26 17:26:46 +0800

    first commit

---
+++ src/main/java/org/pinocchio/study/factory/basic/Animal.java
@@ -0,0 +1,18 @@
+package org.pinocchio.study.factory.basic;
+
+/**
+ * 通用产品接口
+ */
+public interface Animal {
+
+    /**
+     * 吃饭方法
+     */
+    void eat();
+
+    /**
+     * 睡觉方法
+     */
+    void sleep();
+
+}
